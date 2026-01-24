c27894eb94ad4fccb40767cb2ce582696c328704
xzhangfordham <42500181+xzhangfordham@users.noreply.github.com>
2026-01-24 12:09:49 -0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,23 @@
+# Add your name, purpose of the program
+# known bugs here... 
+
+# Define your IsSubString function here
+# remember to write comment before the function 
+
+
+
+# Define your IsSubsequence function here
+# remember to write comment before the function 
+
+
+
+def main():
+    """Main entry point of the program."""
+ 
+    # Test your two functions by calling them with different inputs and 
+    # check their return values against expected value   
+
+
+if __name__ == "__main__":
+    main()
+
